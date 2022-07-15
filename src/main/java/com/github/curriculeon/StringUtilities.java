fa2292cfa50bcc5a394cbc6f6a06dbf274b815da
Eblackk <aps14blacerig@gmail.com>
2022-07-14 22:49:37 -0400

    Get Second Word

--- src/main/java/com/github/curriculeon/StringUtilities.java
+++ src/main/java/com/github/curriculeon/StringUtilities.java
@@ -90,7 +90,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString) {
-        return null;
+        String[] splitWord = spaceDelimitedString.split(" ");
+        return splitWord[1];
     }
 
     /**
